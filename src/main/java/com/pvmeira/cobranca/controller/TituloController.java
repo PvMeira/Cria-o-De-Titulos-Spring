df21550c0472aee17c54396048609440a8af5fed
PvMeira <pv.ciria@gmail.com>
2016-08-02 21:46:17 -0300

    Adicionando interface para tranasações com jpa, no momento utilizando banco H2

--- src/main/java/com/pvmeira/cobranca/controller/TituloController.java
+++ src/main/java/com/pvmeira/cobranca/controller/TituloController.java
@@ -1,14 +1,18 @@
 package com.pvmeira.cobranca.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.pvmeira.cobranca.model.Titulo;
+import com.pvmeira.cobranca.repository.Titulos;
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+	@Autowired
+	Titulos titulos;
 
 	@RequestMapping("/novo")
 	public String novo() {
@@ -17,6 +21,7 @@ public class TituloController {
 
 	@RequestMapping(method = RequestMethod.POST)
 	public String Salvar(Titulo titulo) {
+		titulos.save(titulo);
 		System.out.println("TESTE" + titulo.getDescricao());
 		return "CadastroTitulo";
 	}
---
+++ src/main/java/com/pvmeira/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.pvmeira.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pvmeira.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long> {
+
+}
