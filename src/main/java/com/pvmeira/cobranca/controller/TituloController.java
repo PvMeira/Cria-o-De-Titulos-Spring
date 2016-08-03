e4d1df1490f705a1523065e07c0f8c896853c29d
PvMeira <pv.ciria@gmail.com>
2016-08-02 22:51:50 -0300

    Trocando tag <select> por um loop do tymeleaf e uilizando @modelAtrinute

--- src/main/java/com/pvmeira/cobranca/controller/TituloController.java
+++ src/main/java/com/pvmeira/cobranca/controller/TituloController.java
@@ -1,11 +1,16 @@
 package com.pvmeira.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.pvmeira.cobranca.model.StatusTitulo;
 import com.pvmeira.cobranca.model.Titulo;
 import com.pvmeira.cobranca.repository.Titulos;
 
@@ -16,8 +21,9 @@ public class TituloController {
 	Titulos titulos;
 
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
@@ -26,5 +32,10 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Titulo Salvo com sucesso");
 		return mv;
+	}
+
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
 	}
 }
