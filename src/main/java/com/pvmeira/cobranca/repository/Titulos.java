bd7c52bc879743b0f9f302a982156d18eefab67d
PvMeira <pv.ciria@gmail.com>
2016-08-14 01:20:18 -0300

    Adicionando campo de busca funcionad #2

--- src/main/java/com/pvmeira/cobranca/repository/Titulos.java
+++ src/main/java/com/pvmeira/cobranca/repository/Titulos.java
@@ -1,9 +1,12 @@
 package com.pvmeira.cobranca.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pvmeira.cobranca.model.Titulo;
 
 public interface Titulos extends JpaRepository<Titulo, Long> {
+	public List<Titulo>findByDescricaoContaining(String descricao);
 
 }
